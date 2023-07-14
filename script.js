ca97ee139ff254cd07244cebfee9341e689b96f9
James Weah <jweah@aggies.ncat.edu>
2023-07-14 12:11:26 -0400

    style: Adjusted screen to small screen

---
+++ script.js
@@ -0,0 +1,41 @@
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let selectedCity = document.querySelector('.searches');
+let cityName = document.querySelector('.cit-name');
+let genCityInfo = document.querySelector('.city-spacifics');
+let cityTemp = document.querySelector('temp');
+let cityWind = document.querySelector('wind');
+let cityHumidity = document.querySelector('humidity');
+let dayforCast = document.querySelector('h3');
+//day1
+let cityDate1 = document.querySelector('city-date1');
+let forcastPic1 = document.querySelector('forcast-pic1');
+let temp1 = document.querySelector('temp1');
+let wind1 = document.querySelector('wind1');
+let humidity = document.querySelector('humidity1');
+//day2
+let cityDate2 = document.querySelector('city-date2');
+let forcastPic2 = document.querySelector('forcast-pic2');
+let temp2 = document.querySelector('temp2');
+let wind2 = document.querySelector('wind2');
+let humidity2 = document.querySelector('humidity2');
+//day3
+let cityDate3 = document.querySelector('city-date3');
+let forcastPic3 = document.querySelector('forcast-pic3');
+let temp3 = document.querySelector('temp3');
+let wind3 = document.querySelector('wind3');
+let humidity3 = document.querySelector('humidity3');
+//day4
+let cityDate4 = document.querySelector('city-date4');
+let forcastPic4 = document.querySelector('forcast-pic4');
+let temp4 = document.querySelector('temp4');
+let wind4 = document.querySelector('wind4');
+let humidity4 = document.querySelector('humidity4');
+//day5
+let cityDate5 = document.querySelector('city-date5');
+let forcastPic5 = document.querySelector('forcast-pic5');
+let temp5 = document.querySelector('temp5');
+let wind5 = document.querySelector('wind5');
+let humidity5 = document.querySelector('humidity5');
+
+let apiKey = b8ab3050f479607da13473f115579260;
